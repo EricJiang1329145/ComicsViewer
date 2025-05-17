6cdeea1d1c8131036b348cecc9292f813291d843
EricJiang1329145 <jmr_eric@outlook.com>
2025-05-17 15:25:18 +0800

    Initial Commit

---
+++ ComicsViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComicsViewer
+//
+//  Created by Eric Jiang on 2025/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ComicsViewerTests/ComicsViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsViewerTests.swift
+//  ComicsViewerTests
+//
+//  Created by Eric Jiang on 2025/5/17.
+//
+
+import Testing
+@testable import ComicsViewer
+
+struct ComicsViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
